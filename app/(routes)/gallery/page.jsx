d706ca110aa17f84d3788166a5dc3e3583af4ad3
sujal-2806 <srdwivedi_b23@tx.vjti.ac.in>
2024-02-06 12:51:41 +0530

    added home to navbar

---
+++ app/(routes)/gallery/page.jsx
@@ -0,0 +1,13 @@
+import Navbar from "@/app/components/navbar";
+import Link from "next/link";
+export default function Home() {
+  return (
+    <div className="bg-black">
+      <Navbar/>
+      
+      <p>
+        gallery
+      </p>
+    </div>
+  );
+}
