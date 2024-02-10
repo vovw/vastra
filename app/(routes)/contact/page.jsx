ee3bdafb5761f9d3d38cf0212bd1ed46dff08125
sujal-2806 <srdwivedi_b23@tx.vjti.ac.in>
2024-02-10 16:49:21 +0530

    contact us

--- app/(routes)/contact/page.jsx
+++ app/(routes)/contact/page.jsx
@@ -1,12 +1,26 @@
 import Navbar from "@/app/components/navbar";
+import Head from 'next/head';
 
-export default function Home() {
+const ContactUsPage = () => {
   return (
-    <div className="bg-black">
+    <div className="container mx-auto py-8">
       <Navbar/>
-      <p>
-        contact
-      </p>
+      <Head>
+        <title>Contact Us</title>
+      </Head>
+      <div className="text-center mb-6">
+        <h1 className="text-4xl font-bold">Contact Us</h1>
+        <p className="text-gray-600 font-bold">Get in touch with us for any inquiries or support.</p>
+
+      </div>
+      <div className="max-w-md mx-auto bg-black p-8 rounded-lg shadow-md">
+        <h2 className="text-2xl font-bold mb-4">Contact Information</h2>
+        <p className="mb-4">Email: contact@example.com</p>
+        <p className="mb-4">Name</p>
+        <p>Address</p>
+      </div>
     </div>
   );
-}
+};
+
+export default ContactUsPage;
