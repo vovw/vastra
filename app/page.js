e894c526bf499fa3e1063aa94ee9c2b2892a919a
atharva <voidz7447@gmail.com>
2024-02-13 00:37:07 +0530

    aaa

--- app/page.js
+++ app/page.js
@@ -19,8 +19,6 @@ export default function Home() {
           <br/>
           <br/>
           <br/>
-          <br/>
-          <br/>
           <div className="flex justify-center">
             <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/wgdNa0aKv5k?si=8J8jDvO-rTX3q6QH&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
           </div>
