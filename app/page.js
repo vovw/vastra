0f6010823d37b0f797668724cae57aa5272c765c
atharva <voidz7447@gmail.com>
2024-03-08 18:48:15 +0530

    fixded lading page

--- app/page.js
+++ app/page.js
@@ -6,19 +6,28 @@ import Footer from "./components/fotter";
 const LandingPage = () => {
   return (
     <>
-      <main className="background">
+      <main className="background ">
         <Navbar />
-        <div className="landingPage">
-        <Image 
+        <div className="landingPage py-40">
+          <div className="landingContent flex items-center justify-center">
+            <Image
               className="image"
               src="/logo.png"
               alt="Vastra '24 Logo"
               width={200}
               height={200}
             />
+          </div>
           <div className="landingContent">
-            <h1 style={{ fontFamily: "Cambria" }}>VASTRA &apos;24 </h1> 
-            <h1 style={{ fontFamily: "cursive" }}>Elevate Fashion with Technology</h1>
+            <h1
+              className="text-6xl font-bold drop-shadow-2xl"
+              style={{ fontFamily: "" }}
+            >
+              VASTRA &apos;24{" "}
+            </h1>
+            <h1 className="text-xl" style={{ fontFamily: "" }}>
+              Elevate Fashion with Technology
+            </h1>
           </div>
         </div>
         {/*
